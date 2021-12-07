ee77b2df9dfac908304478a5ff9f22411cd74e73
ginnaaceror <ginnaaceror@gmail.com>
2021-12-07 11:04:10 -0500

    Add heading template

---
+++ components/header.js
@@ -0,0 +1,30 @@
+const header = `
+<section>
+  <h1 tabindex="0">Make Huge Moves.</h1>
+
+    <label for="lenguages">Lenguage</label>
+      <select name="lenguages" id="lenguages">
+        <option value="0">ES</option>
+        <option value="1">EN</option>
+      </select>
+
+    <label for="currencies">Currency</label>
+      <select name="currencies" id="currencies">
+        <option value="0">USD</option>
+        <option value="1">COP</option>
+      </select>
+
+  <div>
+    <img src="images/logo.png" alt="Huge logo" tabindex="0">
+
+    <nav>
+      <a href="#program">Program</a>
+      <a href="#tech">Tech</a>
+      <a href="#talent">Talent</a>
+    </nav>
+
+  </div>
+</section>
+`;
+
+export default header;
