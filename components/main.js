aef9fb84bee09f4811547e340e145dc8c693ca71
ginnaaceror <ginnaaceror@gmail.com>
2021-12-09 16:02:10 -0500

    improvement: add role attribute

--- components/main.js
+++ components/main.js
@@ -1,38 +1,38 @@
 const main = `
-<section>
-  <img src="assets/banner-image.jpeg" alt="Banner image, Sunset New York." tabindex="0">
+<main>
+  <img role="banner" src="assets/banner-image.jpeg" alt="Banner image, Sunset New York." tabindex="0">
 
-  <article id="program" tabindex="0">
-    <h2>PROGRAM</h2>
-    <img src="assets/image1.jpeg" alt="Program" style="max-width:100vh">
+  <article id="program">
+    <h2 tabindex="0">PROGRAM</h2>
+    <img src="assets/image1.jpeg" alt="Sky with stars" style="max-width:100vh">
   </article>
 
-  <article id="tech" tabindex="0">
-    <h2>TECH</h2>
-    <img src="assets/image2.jpeg" alt="Tech" style="max-width:100vh">
+  <article id="tech">
+    <h2 tabindex="0">TECH</h2>
+    <img src="assets/image2.jpeg" alt="People with cellphones" style="max-width:100vh">
   </article>
 
-  <article id="talent" tabindex="0">
-    <h2>TALENT</h2>
-    <img src="assets/image3.jpeg" alt="Talent" style="max-width:100vh">
+  <article id="talent">
+    <h2 tabindex="0">TALENT</h2>
+    <img src="assets/image3.jpeg" alt="Talent people" style="max-width:100vh">
   </article>
 
-  <form>
-    <label for="name">Name:</label>
+  <form role="contact-form">
+    <label role="textfield" for="name">Name:</label>
     <input type="text" id="name" name="name" placeholder="Enter your name"><br><br>
   
-    <label for="lname">Last name:</label>
+    <label role="textfield" for="lname">Last name:</label>
     <input type="text" id="lname" name="lname" placeholder="Enter your last name"><br><br>
   
-    <label for="email">Enter your email:</label>
+    <label role="textfield" for="email">Enter your email:</label>
     <input type="email" id="email" name="email" placeholder="Enter your email"><br><br>
   
-    <label>Comments:</label><br><br>
+    <label role="textarea">Comments:</label><br><br>
     <textarea name="comments" rows="12" cols="35">Send your comments to us.</textarea><br>
     
-    <input type="submit" value="Submit">
+    <input role="button" type="submit" value="Submit">
   </form>
-</section>
+</main>
 `;
 
 export default main;
