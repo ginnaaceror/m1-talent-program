807bdbc32c2f683bcaf14038daf586dde762cb1d
ginnaaceror <ginnaaceror@gmail.com>
2021-12-19 16:18:17 -0500

    add order of attributes in the html

--- components/header.js
+++ components/header.js
@@ -1,16 +1,16 @@
 const header = `
 <header>
   <h1 tabindex="0">Make Huge Moves.</h1>
-  
+
   <div role="regional-menu">
     <label for="lenguages" role="lenguage-list">Lenguage</label>
-      <select name="lenguages" id="lenguages">
+      <select id="lenguages" name="lenguages">
         <option value="0">ES</option>
         <option value="1">EN</option>
       </select>
 
     <label for="currencies" role="currency-list">Currency</label>
-      <select name="currencies" id="currencies">
+      <select id="currencies" name="currencies">
         <option value="0">USD</option>
         <option value="1">COP</option>
       </select>
--- components/main.js
+++ components/main.js
@@ -1,36 +1,36 @@
 const main = `
 <main>
-  <img role="banner" src="assets/banner-image.jpeg" alt="Banner image, Sunset New York." tabindex="0">
+  <img src="assets/banner-image.jpeg" alt="Banner image, Sunset New York." role="banner" tabindex="0">
 
   <article id="program">
     <h2 tabindex="0">PROGRAM</h2>
-    <img src="assets/image1.jpeg" alt="Sky with stars" style="max-width:100vh">
+    <img src="assets/image1.jpeg" alt="Sky with stars" style="max-width:100vh" tabindex="0">
   </article>
 
   <article id="tech">
     <h2 tabindex="0">TECH</h2>
-    <img src="assets/image2.jpeg" alt="People with cellphones" style="max-width:100vh">
+    <img src="assets/image2.jpeg" alt="People with cellphones" style="max-width:100vh" tabindex="0">
   </article>
 
   <article id="talent">
     <h2 tabindex="0">TALENT</h2>
-    <img src="assets/image3.jpeg" alt="Talent people" style="max-width:100vh">
+    <img src="assets/image3.jpeg" alt="Talent people" style="max-width:100vh" tabindex="0">
   </article>
 
   <form role="contact-form">
-    <label role="textfield" for="name">Name:</label>
-    <input type="text" id="name" name="name" placeholder="Enter your name"><br><br>
+    <label for="name" role="textfield">Name:</label>
+    <input id="name" name="name" type="text" placeholder="Enter your name"><br><br>
   
-    <label role="textfield" for="lname">Last name:</label>
-    <input type="text" id="lname" name="lname" placeholder="Enter your last name"><br><br>
+    <label for="lname" role="textfield">Last name:</label>
+    <input id="lname" name="lname" type="text" placeholder="Enter your last name"><br><br>
   
-    <label role="textfield" for="email">Enter your email:</label>
-    <input type="email" id="email" name="email" placeholder="Enter your email"><br><br>
+    <label for="email" role="textfield">Enter your email:</label>
+    <input id="email" name="email" type="email" placeholder="Enter your email"><br><br>
   
     <label role="textarea">Comments:</label><br><br>
     <textarea name="comments" rows="12" cols="35">Send your comments to us.</textarea><br>
     
-    <input role="button" type="submit" value="Submit">
+    <input type="submit" role="button" value="Submit">
   </form>
 </main>
 `;
